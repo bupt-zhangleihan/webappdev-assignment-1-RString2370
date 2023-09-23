0bf9f8db54276decbcef09519ae28d049fdd0aac
RString2370 <2518276414@qq.com>
2023-09-23 13:33:22 +0800

    Changes to be committed: 	modified:   js code demo.js

---
+++ js code demo.js
@@ -0,0 +1,24 @@
+function greeting() {
+    console.log('Hello, world!');
+  }
+greeting();
+
+let variable1=3;
+let variable2=4;
+variable1+variable2;
+
+let money=13000;
+let laptopPrice=10999;
+if (money >= laptopPrice){
+    console.log("Getting a new laptop.");
+}
+else{
+    console.log("Can't afford a new laptop.");
+}
+
+let colors=['red','orange','yellow','green'];
+colors[3]
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
